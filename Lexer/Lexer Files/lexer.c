0f53b61c6add6d154213c457368dfc9f9df41482
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2024-09-03 16:55:30 +0100

    changed files

--- Lexer/Lexer Files/lexer.c
+++ Lexer/Lexer Files/lexer.c
@@ -1,79 +1,394 @@
-/************************************************************************
-University of Leeds
-School of Computing
-COMP2932- Compiler Design and Construction
-Lexer Module
-
-I confirm that the following code has been developed and written by me and it is entirely the result of my own work.
-I also confirm that I have not copied any parts of this program from another person or any other source or facilitated someone to copy this program from me.
-I confirm that I will not publish the program online or share it with anyone without permission of the module leader.
-
-Student Name:
-Student ID:
-Email:
-Date Work Commenced:
-*************************************************************************/
-
-
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include "lexer.h"
 
+FILE* inputFile;
 
-// YOU CAN ADD YOUR OWN FUNCTIONS, DECLARATIONS AND VARIABLES HERE
+char* nameOfFile;
+int line;
+int slashCommentAdded = 0;
+int starCommentAdded = 0; 
+Token globalErrorToken;
 
-
-
-
-
-
-// IMPLEMENT THE FOLLOWING functions
-//***********************************
-
-// Initialise the lexer to read from source file
-// file_name is the name of the source file
-// This requires opening the file and making any necessary initialisations of the lexer
-// If an error occurs, the function should return 0
-// if everything goes well the function should return 1
-int InitLexer (char* file_name)
-{
-  return 0;
+int InitLexer (char* inputDirectory){
+	nameOfFile = inputDirectory;
+	line = 1;
+    inputFile = fopen(inputDirectory, "r");
+	if (inputFile == 0)
+	{
+		printf("Unable to open source file\n");
+		return 0;
+	}
+	return 1;
 }
 
-
-// Get the next token from the source file
-Token GetNextToken ()
-{
-	Token t;
-  t.tp = ERR;
-  return t;
+char whiteSpaceRemover(char c){
+	while (isspace(c) != 0){
+		if (c == '\n') {
+            line++;
+        }
+		c = getc(inputFile);
+    }
+	return c;
 }
 
-// peek (look) at the next token in the source file without removing it from the stream
-Token PeekNextToken ()
-{
-  Token t;
-  t.tp = ERR;
-  return t;
+Token EOFError(Token token){
+	strcpy(token.fl, nameOfFile);
+	token.tp = EOFile;
+	token.lx[0] = '\0';
+	token.ln=line;
+	return token;
 }
 
-// clean out at end, e.g. close files, free memory, ... etc
-int StopLexer ()
-{
+Token EOFInCommentError(Token token){
+	token.tp = ERR;
+	strcpy(token.fl, nameOfFile);
+	strcpy(token.lx, "Error: unexpected eof in comment");
+	token.ec = EofInCom;
+	token.ln=line;
+	return token;
+}
+
+Token RESWORDToken(Token token, char* temp){
+	token.tp = RESWORD;
+	strcpy(token.lx, temp);
+	strcpy(token.fl, nameOfFile);
+	token.ln=line;
+	return token;
+}
+
+Token NewLineInStringError(Token token){
+	token.tp = ERR;
+	strcpy(token.fl, nameOfFile);
+	strcpy(token.lx, "Error: new line in string constant");
+	token.ec = NewLnInStr;
+	token.ln=line;
+	return token;
+}
+
+Token EOFInStringError(Token token){
+	token.tp = ERR;
+	strcpy(token.lx, "Error: unexpected eof in string constant");
+	token.ec = EofInStr;
+	strcpy(token.fl, nameOfFile);
+	token.ln=line;
+	return token;
+}
+
+Token StringToken(Token token, char* temp){
+	token.tp = STRING;
+	strcpy(token.lx, temp);
+	strcpy(token.fl, nameOfFile);
+	token.ln=line;
+	return token;
+}
+
+Token IDToken(Token token, char* temp){
+	token.tp = ID;
+	strcpy(token.lx, temp);
+	strcpy(token.fl, nameOfFile);
+	token.ln=line;
+	return token;
+}
+
+Token SymbolToken(Token token){
+	token.tp = SYMBOL;
+	strcpy(token.fl, nameOfFile);
+	token.ln=line;
+	return token;
+}
+
+Token illegalSymbolError(Token token){
+	token.tp = ERR;
+	strcpy(token.fl, nameOfFile);
+	strcpy(token.lx, "Error: illegal symbol in source file");
+	token.ln=line;
+	return token;
+}
+
+char slashComment(char c){
+	if (c=='/'){
+		c = getc(inputFile);
+		if (c=='/'){
+			// this is a valid comment
+			while (c != '\n'){
+				c = getc(inputFile);
+				if (c == EOF){
+					return EOF;
+				}
+			}
+			// this character has to be new line so get a new character and return it
+			c = getc(inputFile);
+			slashCommentAdded = 1;
+			line++;
+			return c;
+			// the comment is over
+		}
+		else{
+			ungetc(c, inputFile);
+			return '/';
+		}
+	}
+	else{
+		return c;
+	}
+}
+
+char starComment(char c){
+	if (c=='/'){
+		c = getc(inputFile);
+		if (c=='*'){
+			// this is a valid comment
+			while (c != EOF){
+				c = getc(inputFile);
+				if (c == '*'){
+					c = getc(inputFile);
+					if (c == '/'){
+						// the comment is over
+						break;
+					}
+				}
+				// if new line is found, increment line
+				if (c == '\n'){
+					line++;
+				}
+			}
+			// the comment is over
+			if (c == EOF){
+				return EOF;
+			}
+			starCommentAdded = 1;
+			c = getc(inputFile);
+			return c;
+		}
+		else{
+			ungetc(c, inputFile);
+			return '/';
+		}
+	}
+	else{
+		return c;
+	}
+}
+
+Token GetNextToken (){
+    Token token;
+	token.ln=0;
+	int c = getc(inputFile);
+	// Gets spaces
+	c = whiteSpaceRemover(c);
+	// add code to consume comments
+	if (c == EOF)
+	{
+		return EOFError(token);
+	}
+	// 
+	
+	while (c == '/'){
+		c = slashComment(c);
+		if (c == EOF){
+			return EOFInCommentError(token);
+		}
+		if (slashCommentAdded == 1){
+			slashCommentAdded = 0;
+			c = whiteSpaceRemover(c);
+			continue;
+		}
+		c = starComment(c);
+		if (c == EOF){
+			return EOFInCommentError(token);
+		}
+		if (starCommentAdded == 1){
+			starCommentAdded = 0;
+			c = whiteSpaceRemover(c);
+			continue;
+		}
+		break;
+	}
+	// whitespce remover again
+	c = whiteSpaceRemover(c);
+	// check if eof
+	if (c == EOF)
+	{
+		return EOFError(token);
+	}
+
+	char temp[128];
+	int i = 0;
+	// add code to handle strings
+	if (c == '"')
+	{
+		c = getc(inputFile);
+		while (c != '"')
+		{
+			if (c == EOF)
+			{
+				return EOFInStringError(token);
+			}
+			if (c == '\n')
+			{
+				return NewLineInStringError(token);
+			}
+			
+			temp[i++] = c;
+			c = getc(inputFile);
+		}
+		temp[i] = c;
+		temp[i++] = '\0';
+		return StringToken(token, temp);
+	}
+	// add code to handle identifiers and reserved words
+	if (isalpha(c)||(c=='_')){
+		// this is the start of keyword or id
+		while (isalpha(c)||(c=='_')||(isdigit(c)))
+		{
+			temp[i++] = c;
+			c = getc(inputFile);
+		}
+		ungetc(c, inputFile);
+		temp[i] = '\0';
+
+		// check if this is a reserved word
+		if (strcmp(temp, "class") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "constructor") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "function") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "method") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "field") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "static") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "var") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "int") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "char") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "boolean") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "void") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "true") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "false") == 0)
+		{
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "null")==0){
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "this")==0){
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "let")==0){
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "do")==0){
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "if")==0){
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "else")==0){
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "while")==0){
+			return RESWORDToken(token, temp);
+		}
+		else if (strcmp(temp, "return")==0){
+			return RESWORDToken(token, temp);
+		}
+		// if not a reserved word, then it is an id
+		else
+		{
+			return IDToken(token, temp);
+		}
+	}
+	// add code to handle integers
+	if (isdigit(c))
+	{
+		// this is the start of number
+		i = 0;
+		while (isdigit(c))
+		{
+			if ((c == '\n')||(c == EOF)){
+				token.tp = INT;
+				strcpy(token.lx, temp);
+				strcpy(token.fl, nameOfFile);
+				token.ln=line;
+				return token;
+			}
+			temp[i++] = c;
+			c = getc(inputFile);
+		}
+		ungetc(c, inputFile);
+
+		temp[i] = '\0';
+		token.tp = INT;
+		strcpy(token.lx, temp);
+		strcpy(token.fl, nameOfFile);
+		token.ln=line;
+		return token;
+	}
+	// else it must be a symbol
+	if ((c=='(')||(c==')')||(c=='{')||(c=='}')||(c=='[')||(c==']')||(c=='.')||(c==',')||(c==';')||(c=='+')||(c=='-')||(c=='*')||(c=='/')||(c=='&')||(c=='|')||(c=='<')||(c=='>')||(c=='=')||(c=='~')){
+		token.lx[0] = c;
+		token.lx[1] = '\0';
+		return SymbolToken(token);
+	}
+	else{
+		return illegalSymbolError(token);
+	}
+}
+
+Token PeekNextToken (){
+	
+	Token token;
+	token = GetNextToken ();
+	// return the token to the input stream
+	if (token.tp==STRING){
+		ungetc('"', inputFile);
+	}
+	for(int i = strlen(token.lx)-1; i >= 0; i--) {
+		ungetc(token.lx[i], inputFile);
+	}
+	if (token.tp==STRING){
+		ungetc('"', inputFile);
+	}
+	return token;
+}
+
+int StopLexer (){
+	// close the file
+	fclose(inputFile);
 	return 0;
 }
-
-// do not remove the next line
-#ifndef TEST
-int main ()
-{
-	// implement your main function here
-  // NOTE: the autograder will not use your main function
-
-  
-	return 0;
-}
-// do not remove the next line
-#endif
